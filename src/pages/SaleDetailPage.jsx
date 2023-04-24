370ddc44ec3dd3dc91e6c5bc57223f91578755ff
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-04-24 09:08:11 +0200

    pages has been added

---
+++ src/pages/SaleDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SaleDetailPage = () => {
+  return (
+    <div className="sale-detail-page">
+      <h1>Sales Details</h1>
+    </div>
+  );
+};
+
+export default SaleDetailPage;
