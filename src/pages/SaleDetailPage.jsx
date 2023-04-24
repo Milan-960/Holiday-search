f0f09d526917ad48237783c8d93b3c2c59c4d2a9
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-04-24 11:58:29 +0200

    html react parser has been added

--- src/pages/SaleDetailPage.jsx
+++ src/pages/SaleDetailPage.jsx
@@ -1,39 +1,58 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { fetchSaleDetails } from "../api";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { Carousel } from "react-responsive-carousel";
+import parse from "html-react-parser";
+
+import { fetchSaleDetails } from "../api";
 
 const SaleDetailPage = () => {
-  const [details, setDetails] = useState(null);
   const { id } = useParams();
+  const navigate = useNavigate();
+  const [details, setDetails] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchSaleDetails(id);
       setDetails(data);
     };
-
     fetchData();
   }, [id]);
+
+  const goBackToResult = () => {
+    navigate(-1);
+  };
 
   if (!details) {
     return <div>Loading...</div>;
   }
 
+  const {
+    editorial: { title, destinationName },
+    photos,
+    prices: {
+      leadRate: { forDisplay },
+    },
+  } = details;
+
+  const renderHotelDetails = () => {
+    return parse(details.editorial.hotelDetails);
+  };
+
   return (
     <div className="sale-detail-page">
       <h1>Hotel Details</h1>
-      <h2>{details.editorial.title}</h2>
-      <p>{details.editorial.destinationName}</p>
-      {details.photos.length > 0 && (
+      <Link onClick={goBackToResult}>Go back</Link>
+      <h1>{title}</h1>
+      <p>{destinationName}</p>
+      {photos.length > 0 && (
         <div className="carousel-container">
           <Carousel>
-            {details.photos.map((photo, index) => (
+            {photos.map((photo, index) => (
               <div key={index}>
                 <img
                   className="carousel-img"
                   src={photo.url}
-                  alt={`${details.editorial.title}-${index}`}
+                  alt={`${title}-${index}`}
                 />
               </div>
             ))}
@@ -43,11 +62,11 @@ const SaleDetailPage = () => {
       <div className="detail-box">
         <div>
           <h2>Hotel Details</h2>
-          <p>{details.editorial.hotelDetails}</p>
+          <div className="hotel-details-content">{renderHotelDetails()}</div>
         </div>
         <div>
           <h2>Price</h2>
-          <p>{details.prices.leadRate.forDisplay}</p>
+          <p>{forDisplay}</p>
         </div>
       </div>
     </div>
